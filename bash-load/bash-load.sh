2a78ad2096bc8280d0cb6ec1a6817b16f110d8b8
Jh Hu <hu.jianhong_2008@163.com>
2018-10-05 18:02:51 +0800

    modify begin/end mark

--- bash-load/bash-load.sh
+++ bash-load/bash-load.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-# BEGIN
+### BEGIN BASH LOAD
+function cat-script() {
+    local mark_bgn mark_end
+
+    # begin/end mark
+    mark_bgn='### BEGIN BASH LOAD'
+    mark_end='### END BASH LOAD'
+
+    sed -n "/^${mark_bgn}$/,/^${mark_end}$/p" "$1"
+}
+
 function load() {
     local func="$1"
     local fname=${func}.sh
@@ -9,8 +19,7 @@ function load() {
     echo "load finished"
     echo
 }
-
-# END
+### END BASH LOAD
 
 # self echo
-sed -n '/^# BEGIN/,/^# END/p' "$0"
+cat-script "$0"
